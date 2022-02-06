b09f188867c874326d7b3f64872e3992fce697fd
Arthur VINOT <vinot.arthur@outlook.com>
2022-02-06 23:18:33 +0100

    Feature 1 & 2

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -117,8 +117,6 @@ selectShow.addEventListener('change', async (event) => {
   render(currentProducts, currentPagination);
 });
 
-currentPagination.setCurrentProducts()
-
 selectPage.addEventListener('change', event => {
   fetchProducts(parseInt(event.target.value), currentPagination.pageSize)
     .then(setCurrentProducts)
